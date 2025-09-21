996d4fafac566f4f9aeb208070e4fbbd303dd815
Mukta01 <muktavarak@gmail.com>
2025-09-21 19:14:21 +0530

    repeats hello till

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int n ;
+    printf("Enter n:\n");
+    scanf("%d",&n);
+    for(int i = 0 ; i<n ; i++){
+        printf("\n%d. Hello world",i+1);
+    }
+    
+}
